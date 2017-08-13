745d57bc99e45783cc551216f2671eab12dce738
janvarga94 <janvarg94@gmail.com>
2017-08-13 19:26:43 +0200

    init

---
+++ WebApplication2/JanHub.cs
@@ -0,0 +1,81 @@
+﻿using HtmlAgilityPack;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2
+{
+    public class JanHub : Hub
+    {
+        private static HtmlDocument doc;
+
+        public JanHub() : base()
+        {
+            if(doc == null)
+            {
+                doc = new HtmlDocument();
+                doc.LoadHtml(@"<body/>");
+            }
+
+        }
+
+        public object getInitData()
+        {
+            return new { connectionId =  Context.ConnectionId, currentDocument = doc.DocumentNode.SelectSingleNode("//body").InnerHtml };
+        }
+
+        public void addElement(string htmlElement, string parentId)
+        {
+
+            var docy = new HtmlDocument();
+            docy.LoadHtml(htmlElement);
+            var newElement = docy.DocumentNode.ChildNodes[0];
+
+            if (string.IsNullOrWhiteSpace(parentId))
+            {
+                HtmlNode body = doc.DocumentNode.SelectSingleNode("//body");
+                body.PrependChild(newElement);
+            }
+            else
+            {
+                var parent = doc.GetElementbyId(parentId);
+                if (parent != null)
+                    parent.PrependChild(newElement);
+            }
+
+            var outer = doc.DocumentNode.OuterHtml;
+
+            Clients.Others.elementAdded(new { parentId = parentId, newElement = htmlElement });
+        }
+
+        public void removeElement(string elementId)
+        {
+
+            var element = doc.GetElementbyId(elementId);
+            if (element != null && element.Name != "body") {
+                element.Remove();
+                Clients.Others.elementRemoved(elementId);
+            } 
+
+        }
+
+        public void updateElement(string htmlElement)
+        {
+            var docy = new HtmlDocument();
+            docy.LoadHtml(htmlElement);
+
+            var newChild = docy.DocumentNode.ChildNodes[0];
+            var idOfNewChild = newChild.Id;
+
+            var oldChild = doc.DocumentNode.SelectSingleNode($"//*[@id={newChild.Id}]");
+            if(newChild != null && oldChild != null)
+            {
+                var parent = oldChild.ParentNode;
+                    parent.ReplaceChild(newChild, oldChild);
+                Clients.Others.elementUpdated(htmlElement);
+            }
+        }
+    }
+}
---
+++ readme.txt
@@ -0,0 +1,16 @@
+requirements: npm, visual studio 2017 preview, bower
+extensions: npm task runner, gulp task runner
+
+setup automatic bundling on file changes : 
+	right click on bundle config and click 'convert to gulp', something like that
+	go to npm task runner and install dependencies
+	go to gulp task runner, expand it, set 'watch' task to run on project open
+
+
+
+
+
+
+notes--------------------
+
+	if you put <!doctype html> in index page, things wont work, css will make problems when you dont use px, just plain number
